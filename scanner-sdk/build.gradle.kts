18932fa587fda8c215eff5c434ff24c4309bbb69
Gowtham <gowthamvenkatasalam33@gmail.com>
2025-12-15 23:54:23 +0530

    app removed

--- scanner-sdk/build.gradle.kts
+++ scanner-sdk/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     }
 }
 
+group = "com.github.Gowthamgsv32"
+version = "1.0.0"
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
